b39c1ec5065a426e2f4eaf74430d375504cf2f45
Elize <0891435@hr.nl>
2015-11-18 18:49:44 +0100

    Okee, de data wordt als het goed is wel ingelezen, alleens soms werkt de looper niet helemaal ofzo:/

--- DataVerwerkingsSysteem/src/DatabaseClasses/Database_Manager.java
+++ DataVerwerkingsSysteem/src/DatabaseClasses/Database_Manager.java
@@ -5,15 +5,19 @@
  */
 package DatabaseClasses;
 
+import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceException;
 import javax.swing.JOptionPane;
 
 /**
@@ -22,13 +26,12 @@ import javax.swing.JOptionPane;
  */
 public class Database_Manager extends Thread {
     
-    private static ArrayList<Object> objectsToPersist = new ArrayList();
+    private static ArrayList<EntityClass> objectsToPersist = new ArrayList();
         
     private static EntityManagerFactory entityManagerFactory
               = Persistence.createEntityManagerFactory("DataVerwerkingsSysteemPU");
     
     
-
     public static Object find(Class classToFind, Object objectToFind){
     
         Object foundObject = null;
@@ -49,64 +52,73 @@ public class Database_Manager extends Thread {
        
     @Override
     public void run() {
-        super.run();
-       
+               
         while(true){
             
             if(!objectsToPersist.isEmpty()){
-                persist(objectsToPersist.get(0));
-            }else{
-            
+               persistOrMergeObject(objectsToPersist.get(0));
+               
             }
+            }
+    
+              
+    }
+    
+    private void persistOrMergeObject(EntityClass object){
+    
+        try{
+            EntityManager entityManager = entityManagerFactory.createEntityManager();
+            entityManager.getTransaction().begin();
+            
+            if(entityManager.find(object.getClass(),  object.getPK()) != null){
+                merge(object, entityManager);
+                
+            }else{
+               persist(object, entityManager);
+            }
+         
+         
+            entityManager.getTransaction().commit();
+            
+            entityManager.clear();
+            entityManager.close();
+            objectsToPersist.remove(object);
           
+        }catch(PersistenceException ex){
+            System.out.println(ex);
+        
+        }catch(Exception ex){
+            System.out.println(ex);
         }
+         
+         
+    }
+    
+    private void merge(EntityClass object, EntityManager entityManager){
+       
+       
+       checkIfObjectHasCar(object, entityManager);
+       entityManager.merge(object);
+      
+    
     }
     
     /**
      * This is the persist method used by alle entities;
      * @param object: the entity to be inserted. 
      */
-    public void persist(Object object) {
-       
-       
-       try{
-           
-       EntityManager entityManager = entityManagerFactory.createEntityManager();
-       entityManager.getTransaction().begin();
-       
-       /**
-        * Since not all the entity classes have a getCar() method,
-        * we are going to check if it has one first
-        * If so, we are going to check if the car needs to be inserted into the database.
-        */
-        try {
-            Class noParams[] = {};
-            Class[] paramString = new Class[1];	
-            paramString[0] = String.class;
-            Car car = (Car) object.getClass().getDeclaredMethod("getCar", noParams).invoke(object, null);
-            insertCarIfNeeded(car, entityManager);
-        } catch (Exception ex) {
-            System.out.println(ex);
-        }
+    public void persist(EntityClass object, EntityManager entityManager){
+              
+       checkIfObjectHasCar(object, entityManager);
        entityManager.persist(object);
        
-       entityManager.getTransaction().commit();
-         
-       entityManager.clear();
-       entityManager.close();
-          
-       }catch(Exception ex){
-       System.out.println(ex);
-       }finally{
-       objectsToPersist.remove(object);
-       }
     }
 
-    public static void addObjectToPersistList(Object object) {
+    public static void addObjectToPersistList(EntityClass object) {
         objectsToPersist.add(object);       
     }
     
-    public static void addObjectToPersistListAtFirstPosition(Object object){
+    public static void addObjectToPersistListAtFirstPosition(EntityClass object){
         objectsToPersist.add(0, object);
     }
 
@@ -115,12 +127,22 @@ public class Database_Manager extends Thread {
             em.persist(car);
         }
     }
-
- 
-
+      /**
+        * Since not all the entity classes have a getCar() method,
+        * we are going to check if it has one first
+        * If so, we are going to check if the car needs to be inserted into the database.
+        */
+    private void checkIfObjectHasCar(EntityClass object, EntityManager entityManager) {
+        try {
+            
+            Car car = (Car) object.getCar();
+            if(car != null){
+            insertCarIfNeeded(car, entityManager);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
 
     
-    
-   
-   
 }
