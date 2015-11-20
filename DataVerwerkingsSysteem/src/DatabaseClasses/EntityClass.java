85c005dc8c57c96bdea6851a57554807f8d82dc8
Elize <0891435@hr.nl>
2015-11-20 08:53:13 +0100

    EntityClass geadd

---
+++ DataVerwerkingsSysteem/src/DatabaseClasses/EntityClass.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DatabaseClasses;
+
+/**
+ *
+ * @author Elize
+ */
+public interface EntityClass {
+    
+    /**
+     * Get the primary key of the entity object.
+     * @return the PK
+     */
+    Object getPK();
+    
+    /**
+     * Get the car the entitiy object.
+     * @return if the object has a car, the car.
+     * Otherwise, null;
+     */
+    
+    Car getCar();
+    
+    /**
+     * Method for merging the current object with the object on the database.
+     * If a field is not null, and not te same, the value is updated.
+     * @param ec the object from te database.
+     * @return object to merge on database.
+     */
+    EntityClass mergeWithObjectFromDatabase(EntityClass ec);
+    
+    
+    
+    
+}
